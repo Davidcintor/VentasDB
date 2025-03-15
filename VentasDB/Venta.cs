cedfe6b639fb2f737298c6b20f570a0d79d831bb
david <david@DAVIDCINTO>
2025-03-15 13:51:23 -0700

    Add project files.

---
+++ VentasDB/Venta.cs
@@ -0,0 +1,11 @@
+﻿namespace VentasDB
+{
+    internal class Venta
+    {
+        public int Id { get; set; }
+        public string Cliente { get; set; }
+        public decimal Total { get; set; }
+
+        public List<VentaConcepto> Conceptos { get; set; }
+    }
+}
---
+++ VentasDB/VentaConcepto.cs
@@ -0,0 +1,12 @@
+﻿namespace VentasDB
+{
+    internal class VentaConcepto
+    {
+        public int Id { get; set; }
+        public int VentaId { get; set; }
+        public int Cantidad { get; set; }
+        public string Descripcion { get; set; }
+        public decimal PrecioUnitario { get; set; }
+        public decimal Importe { get; set; }
+    }
+}
